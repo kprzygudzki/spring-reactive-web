90f7e39d94fb01a871679e6b4e9f4d64f0aa76eb
Krzysiek Przygudzki <kpr@touk.pl>
2018-01-31 18:05:18 +0100

    Initial commit

---
+++ src/main/java/pl/przygudzki/baeldung/testreactiveweb/TestReactiveWebApplication.java
@@ -0,0 +1,13 @@
+package pl.przygudzki.baeldung.testreactiveweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestReactiveWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestReactiveWebApplication.class, args);
+	}
+
+}
